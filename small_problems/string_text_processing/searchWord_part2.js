d661237812b296ee000e8c606cc81ff29ae183ce
Richard Spitz <richard.spitz@protonmail.ch>
2021-02-28 19:13:00 +0100

    uploading last JS101 string processing exerxise

---
+++ small_problems/string_text_processing/searchWord_part2.js
@@ -0,0 +1,24 @@
+function searchWord(string, text) {
+  return text.split(" ").map(word => {
+    if (word.toUpperCase() === string.toUpperCase()) return highlight(word);
+    else return word;
+  }).join(" ");
+}
+
+function highlight(string) {
+  return "**" + string.toUpperCase() + "**";
+}
+
+function searchWordRegex(string, text) {
+  let regex = new RegExp(string, "gi");
+  return text.replace(regex, "**" + string.toUpperCase() + "**");
+}
+
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?';
+
+console.log(searchWord('sed', text));
+console.log(searchWordRegex('sed', text));
+console.log(searchWordRegex('sed', text) === searchWord('sed', text));
+
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
