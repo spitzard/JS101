3f6c35071d108f36d5f87951685ef5e0c75cc58f
Richard Spitz <richard.spitz@protonmail.ch>
2021-02-28 16:14:24 +0100

    adding replace vowel exercise

---
+++ small_problems/string_text_processing/deleteVowels.js
@@ -0,0 +1,9 @@
+let reg = /[aeiou]/gi;
+
+function removeVowels(arr) {
+  return arr.map(subarr => subarr.replace(reg,""));
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
