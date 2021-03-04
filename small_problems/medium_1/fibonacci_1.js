c0b1162d22f1ff452213305bb0e2a79b5be2bd4d
Richard Spitz <richard.spitz@protonmail.ch>
2021-03-04 19:53:51 +0100

    added some fibonaccis

--- small_problems/medium_1/fibonacci_1.js
+++ small_problems/medium_1/fibonacci_1.js
@@ -1,21 +1,42 @@
 function fibonacci(nth) {
-  if (nth < 1) return 0;
   if (nth <= 2 ) return 1;
-
   return fibonacci(nth - 1) + fibonacci(nth - 2);
 }
 
-
-function recursiveSum(num) {
-  if (num === 1) return 1;
-  return num + recursiveSum(num - 1);
+function proceduralFibonacci(nth) {
+  let F1 = 1;
+  let F2 = 1;
+  let Fn = 0;
+  if (nth <= 2 &&  nth > 0) return 1;
+  for (let pos = 2; pos < nth; pos++) {
+    Fn = F1 + F2;
+    F1 = F2;
+    F2 = Fn;
+  }
+  return Fn;
 }
 
-console.log(recursiveSum(100) === 5050);
+console.log("RECURSIVE");
 console.log(fibonacci(1) === 1);
 console.log(fibonacci(2) === 1);
 console.log(fibonacci(3) === 2);
 console.log(fibonacci(4) === 3);
 console.log(fibonacci(5) === 5);
 console.log(fibonacci(12) === 144);
-console.log(fibonacci(0));
+
+console.log("PROCEDURAL");
+console.log(proceduralFibonacci(1) === 1);
+console.log(proceduralFibonacci(3) === 2);
+console.log(proceduralFibonacci(5) === 5);
+console.log(proceduralFibonacci(5) === 5);
+console.log(proceduralFibonacci(12) === 144);
+
+
+/// example
+function recursiveSum(num) {
+  if (num === 1) return 1;
+  return num + recursiveSum(num - 1);
+}
+console.log(recursiveSum(100) === 5050);
+
+
