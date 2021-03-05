50623b619d1751be1cb918c92d5784a7892ea2bd
Richard Spitz <richard.spitz@protonmail.ch>
2021-03-05 18:27:52 +0100

    improved the triangle input check

--- small_problems/medium_2/triangleSides.js
+++ small_problems/medium_2/triangleSides.js
@@ -1,7 +1,7 @@
 function triangle(a, b, c) {
   let side = [a, b, c].sort();
-
-  if (side[0] + side[1] <= side[2]) return "invalid";
+  let negzero = side.filter(side => side <= 0).length;
+  if (side[0] + side[1] < side[2] || negzero > 0) return "invalid";
   else if (side[0] === side[1] || side[0] === side[2]) return "equilateral";
   else if (side[0] === side[1] || side[0] === side[2] || side[1] === side[2]) {
     return "isosceles";
@@ -13,3 +13,4 @@ console.log(triangle(3, 3, 1.5));      // "isosceles"
 console.log(triangle(3, 4, 5));        // "scalene"
 console.log(triangle(0, 3, 3));        // "invalid"
 console.log(triangle(3, 1, 1));        // "invalid"
+console.log(triangle(-4, -1, -1));        // "invalid"
