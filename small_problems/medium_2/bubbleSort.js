9e2fcc4abefff8d67ebb3cface30c40d45a1114a
Richard Spitz <richard.spitz@protonmail.ch>
2021-03-06 16:06:18 +0100

    adding bubblesort with double loop

---
+++ small_problems/medium_2/bubbleSort.js
@@ -0,0 +1,40 @@
+function bubbleSort(array) {
+  for (let outer = 0; outer < array.length; outer++) {
+    for (let inner = 0; inner < array.length; inner++) {
+      if (array[inner] > array[inner + 1]) {
+        let swapped = array[inner + 1];
+        array[inner + 1] = array[inner];
+        array[inner] = swapped;
+      }
+    }
+  }
+}
+
+function betterBubbelSort(array) {
+  for (let outer = 0; outer < array.length; outer++) {
+    let swapped = false;
+    for (let inner = 0; inner < array.length; inner++) {
+      if (array[inner] > array[inner + 1]) {
+        if (array[inner]  > array[inner + 1]) {
+          [array[inner], array[inner + 1]] = [array[inner + 1], array[inner]];
+          swapped = true;
+        }
+        if (swapped === false) break;
+      }
+    }
+  }
+}
+
+
+
+let array1 = [5, 3];
+bubbleSort(array1);
+console.log(array1);    // [3, 5]
+
+let array2 = [6, 2, 7, 1, 4];
+bubbleSort(array2);
+console.log(array2);    // [1, 2, 4, 6, 7]
+
+let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+bubbleSort(array3);
+console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
